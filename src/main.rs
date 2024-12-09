5acabc3a35306d297b868b386014f5ba503fcd7e
WrongNicholas <mnwojnar2000@gmail.com>
2024-12-08 23:30:26 -0500

    Remove unnecessary JSON deserialization for stages

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,15 @@ use std::io;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let word = fetch_word().await?;
-    let stages = get_stages()?;
+    let stages = get_stages();
     let mut stage_index = 0;
 
     let characters = word.chars().collect::<HashSet<char>>();
-
     let mut guesses: HashSet<char> = HashSet::new();
 
     while stage_index < stages.len() - 1 {
         let revealed_word = get_revealed_word(&word, &guesses);
+
         println!(
             "Guesses:{:?}\n{}\n{}",
             guesses, stages[stage_index], revealed_word
@@ -49,10 +49,18 @@ async fn fetch_word() -> Result<String, Box<dyn Error>> {
         .unwrap())
 }
 
-fn get_stages() -> Result<Vec<String>, Box<dyn Error>> {
-    let file = std::fs::File::open("res/stages.json")?;
-    let stages: Vec<String> = serde_json::from_reader(file)?;
-    Ok(stages)
+fn get_stages() -> Vec<String> {
+    let stages = [
+        "  +---+\n  |   |\n      |\n      |\n      |\n      |\n=========",
+        "  +---+\n  |   |\n  O   |\n      |\n      |\n      |\n=========",
+        "  +---+\n  |   |\n  O   |\n  |   |\n      |\n      |\n=========",
+        "  +---+\n  |   |\n  O   |\n /|   |\n      |\n      |\n=========",
+        "  +---+\n  |   |\n  O   |\n /|\\  |\n      |\n      |\n=========",
+        "  +---+\n  |   |\n  O   |\n /|\\  |\n /    |\n      |\n=========",
+        "  +---+\n  |   |\n  O   |\n /|\\  |\n / \\  |\n      |\n=========",
+    ];
+
+    stages.iter().map(|&s| s.to_string()).collect()
 }
 
 fn prompt_user() -> Result<char, Box<dyn Error>> {
