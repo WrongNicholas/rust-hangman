d5979eb87cdc6f5ff70de7d783f5af77a68fe643
WrongNicholas <mnwojnar2000@gmail.com>
2026-01-30 18:39:48 -0500

    Add screen clearing

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let mut guesses: HashSet<char> = HashSet::new();
 
     while stage_index < stages.len() - 1 {
+        print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
         let revealed_word = get_revealed_word(&word, &guesses);
 
         println!(
