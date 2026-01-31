2c907791786a5a70ab6481ba56903f03c4c80fe8
WrongNicholas <mnwojnar2000@gmail.com>
2026-01-30 22:28:16 -0500

    Add --length parameter

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,31 @@
 use std::collections::HashSet;
 use std::error::Error;
 use std::io;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about = "Example CLI", long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    length: Option<usize>,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let word = fetch_word().await?;
+    let args = Args::parse();
+
+    // gather the URL, adding `length` if between 2-15 inclusive
+    let url = match args.length {
+        Some(length) if (2..=15).contains(&length) => {
+            format!(
+                "https://random-word-api.herokuapp.com/word?length={length}"
+            )
+        }
+        _ => "https://random-word-api.herokuapp.com/word".to_string()
+    };
+
+    // set up game
+    let word = fetch_word(&url).await?;
     let stages = get_stages();
     let mut stage_index = 0;
 
@@ -12,7 +33,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let mut guesses: HashSet<char> = HashSet::new();
 
     while stage_index < stages.len() - 1 {
+
+        // clear screen
         print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+
         let revealed_word = get_revealed_word(&word, &guesses);
 
         println!(
@@ -40,8 +64,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-async fn fetch_word() -> Result<String, Box<dyn Error>> {
-    Ok(reqwest::get("https://random-word-api.herokuapp.com/word")
+// fetches a random word from the given API URL
+async fn fetch_word(url: &str) -> Result<String, Box<dyn Error>> {
+    Ok(reqwest::get(url)
         .await?
         .json::<Vec<String>>()
         .await?
@@ -50,6 +75,7 @@ async fn fetch_word() -> Result<String, Box<dyn Error>> {
         .unwrap())
 }
 
+// returns ASCII art stages to display hangman progress
 fn get_stages() -> Vec<String> {
     let stages = [
         "  +---+\n  |   |\n      |\n      |\n      |\n      |\n=========",
@@ -64,6 +90,7 @@ fn get_stages() -> Vec<String> {
     stages.iter().map(|&s| s.to_string()).collect()
 }
 
+// prompts the user for a single character guess
 fn prompt_user() -> Result<char, Box<dyn Error>> {
     println!("Enter a guess:");
     let mut buffer = String::new();
@@ -71,6 +98,7 @@ fn prompt_user() -> Result<char, Box<dyn Error>> {
     Ok(buffer.chars().next().unwrap())
 }
 
+// returns the partially revealed word based on previous guesses
 fn get_revealed_word(word: &str, guesses: &HashSet<char>) -> String {
     word.chars()
         .map(|c| if guesses.contains(&c) { c } else { '_' })
